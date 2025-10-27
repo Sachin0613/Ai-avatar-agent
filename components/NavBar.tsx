bfd6200728533b9f554de0c481cc8da8f3dbadeb
Sachin Yadav <yadavsachin6001@gmail.com>
2025-10-27 17:43:54 +0530

    Add files via upload
    hi

---
+++ components/NavBar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+
+import { GithubIcon, HeyGenLogo } from "./Icons";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="flex flex-row justify-between items-center w-[1000px] m-auto p-6">
+        <div className="flex flex-row items-center gap-4">
+          <Link href="https://app.heygen.com/" target="_blank">
+            <HeyGenLogo />
+          </Link>
+          <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text">
+            <p className="text-xl font-semibold text-transparent">
+              AI Avatar Agent
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-6">
+          <Link
+            href="https://labs.heygen.com/interactive-avatar"
+            target="_blank"
+          >
+            Avatars
+          </Link>
+          <Link
+            href="https://docs.heygen.com/reference/list-voices-v2"
+            target="_blank"
+          >
+            Voices
+          </Link>
+          <Link
+            href="https://docs.heygen.com/reference/new-session-copy"
+            target="_blank"
+          >
+            API Docs
+          </Link>
+          <Link
+            href="https://help.heygen.com/en/articles/9182113-interactive-avatar-101-your-ultimate-guide"
+            target="_blank"
+          >
+            Guide
+          </Link>
+         
+        </div>
+      </div>
+    </>
+  );
+}
